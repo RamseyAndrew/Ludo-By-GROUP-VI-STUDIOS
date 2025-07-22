851c042ace5b3f470e979cba920c12eff32baf68
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-22 23:09:08 +0300

    insertions made to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
-function App(){
+import { useState } from "react"
 
+function App(){
+    const [diceValue, setDiceValue] = useState(1)
+    const [rolling, setRolling] = useState(false)
+
+    const rollDice = () => {
+        if (rolling) return;
+        setRolling(true)
+
+        const newValue = Math.floor(Math.random()*6 + 1)
+        setTimeout(() => {
+            setDiceValue(newValue)
+            setRolling(false)
+        }, 1000)
+    }
+
+    return(
+        <div className="App">
+            <div className={`dice${rolling ? "rolling":""}`}>{diceValue}</div>
+            <button onClick={rollDice} disabled={rolling}>
+                {rolling ? "Rolling..." : "Roll Dice"}
+            </button>
+        </div>
+    )
 }
 
 export default App

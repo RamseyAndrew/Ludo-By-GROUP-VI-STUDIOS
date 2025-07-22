c5236835a47f0e048aa4f51b266bcfa1c01a1aac
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-07-23 00:54:31 +0300

    TurnManager file created and insertions made

---
+++ src/TurnManager.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+const players = ["Red", "Blue", "Green", "Yellow"];
+const playerColors = {
+    Red: "red",
+    Blue: "blue",
+    Green: "green",
+    Yellow: "yellow",
+}
+
+function TurnManager({currentPlayerIndex, onNextTurn}) {
+    const currentPlayer = players[currentPlayerIndex];
+
+    return(
+        <div className="turn-manager">
+            <h1 style={{color: playerColors[currentPlayer]}}>
+                {currentPlayer}'s Turn
+            </h1>
+
+            <button onClick={onNextTurn} className="next-btn">
+                Next Turn
+            </button>
+        </div>
+    );
+}
+
+export default TurnManager;

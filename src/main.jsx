36542cb276dab02b66f558ae48056cebf92844f3
ramsey900 <ramsey.anyona@student.moringaschool.com>
2025-07-23 15:41:59 +0300

    assed and displayed the ludo board succesfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Board from './Board'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Board/>
   </StrictMode>,
 )
